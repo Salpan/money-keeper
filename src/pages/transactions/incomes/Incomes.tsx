9e55a9c51af3293d7d6e9104e1253ce59d4420fb
Salpan <rus2009ian@gmail.com>
2025-01-28 18:48:51 +0500

    Add Incomes transaction

--- src/pages/transactions/incomes/Incomes.tsx
+++ src/pages/transactions/incomes/Incomes.tsx
@@ -1,4 +1,7 @@
 import { rules } from '_consts/rules';
+import { $budget } from '_models/budget';
+import { addTransactionFx, createTransactionEv } from '_models/transaction';
+import { ExpenseField, TransactionForm } from '_types/transactions';
 import {
     Button,
     DatePicker,
@@ -8,41 +11,48 @@ import {
     Select,
     Typography,
 } from 'antd';
+import { useUnit } from 'effector-react';
 import { FC } from 'react';
 
-type IncomesCreateFields = {
-    amount?: number;
-    date?: string;
-    method?: string;
-};
-
 export const Incomes: FC = () => {
-    const finishHandler = (values: unknown) => {
-        console.log(values);
-    };
+    const budget = useUnit($budget);
+    const isPending = useUnit(addTransactionFx.pending);
 
+    const finishHandler = (values: TransactionForm) => {
+        console.log(values);
+        createTransactionEv({
+            ...values,
+            transaction: 'income',
+            amount: Number(values.amount),
+        });
+    };
     return (
         <Flex gap={30} vertical>
+            <Typography.Title level={4}>
+                Новое поступление {budget?.name}
+            </Typography.Title>
             <Typography.Title level={4}>Новое поступление</Typography.Title>
             <Form size="large" onFinish={finishHandler} layout="vertical">
-                <Form.Item<IncomesCreateFields>
+                <Form.Item<ExpenseField>
                     label="Сумма"
                     name="amount"
                     rules={[rules]}
                 >
                     <Input type="number" />
                 </Form.Item>
-                <Form.Item<IncomesCreateFields> label="Дата" name="date">
+                <Form.Item<ExpenseField> label="Дата" name="date">
                     <DatePicker />
                 </Form.Item>
-                <Form.Item<IncomesCreateFields> label="Способ" name="method">
+                <Form.Item<ExpenseField> label="Способ" name="categories">
                     <Select>
                         <Select.Option value="cash">Наличные</Select.Option>
                         <Select.Option value="transfer">Перевод</Select.Option>
                     </Select>
                 </Form.Item>
                 <Form.Item>
-                    <Button htmlType="submit">Сохранить транзакцию</Button>
+                    <Button loading={isPending} htmlType="submit">
+                        Сохранить транзакцию
+                    </Button>
                 </Form.Item>
             </Form>
         </Flex>
